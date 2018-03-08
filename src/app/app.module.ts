6f18301b0de7bb774b8d294a73a3193f952d408b
Michalik, Joshua <smileyfacepancakes@gmail.com>
2018-03-08 09:25:59 -0500

    Add template details for ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,31 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { reducers, metaReducers } from './store';
+import { AppEffects } from './store/app.effects';
+import { AppComponent } from './app.component';
+import { HomeContainer } from './containers/home.container';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    AppComponent,
+    HomeContainer
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(AppComponent),
+    StoreModule.forRoot(reducers, {metaReducers}),
+    StoreDevtoolsModule.instrument(),
+    EffectsModule.forRoot([AppEffects]),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    AppComponent,
+    HomeContainer
   ],
   providers: [
     StatusBar,
